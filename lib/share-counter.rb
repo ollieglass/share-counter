5a61fc0e1c9d49322102fa732607313d788909d3
Ollie Glass <ollie@makeshift.io>
2013-12-20 15:29:01 +0000

    reddit

--- lib/share-counter.rb
+++ lib/share-counter.rb
@@ -1,60 +1,68 @@
+require 'share-counter/common'
+require 'nokogiri'
+require 'open-uri'
+
 class ShareCounter
 
-  # require 'share-counter/common'
+  # network lookups
 
-  # require 'json'
-  require 'rest-client'
-  require 'nokogiri'
-  require 'open-uri'
+  def self.reddit url
+    html = make_request "http://www.reddit.com/api/info.json", url: url
+    j = JSON.parse(html)
+
+    unless j['data']['children'].empty?
+      return j['data']['children']['data']['score']
+    else
+      return 0
+    end
+  end
 
   def self.twitter url
-    url = "http://urls.api.twitter.com/1/urls/count.json?url=" + URI::encode(url)
-    html = RestClient.get url
+    html = make_request "http://urls.api.twitter.com/1/urls/count.json",  url: url
     return JSON.parse(html)['count']
   end
 
   def self.facebook url
-    url = 'https://api.facebook.com/method/fql.query?format=json&query=select like_count from link_stat where url="' + url + '"'
-    url = URI::encode(url)
-    html = RestClient.get url
+    html = make_request "https://api.facebook.com/method/fql.query", format: "json", query: "select like_count from link_stat where url=\"#{url}\""
     return JSON.parse(html)[0]['like_count']
   end
 
   def self.linkedin url
-    url = "http://www.linkedin.com/countserv/count/share?url=" + URI::encode(url)
-    html = RestClient.get url
-
-    callback = "IN.Tags.Share.handleCount"
-    html = html.gsub(/\A\/\*\*\/\s+/, "").gsub(/^(.*);+\n*$/, "\\1").gsub(/^#{callback}\((.*)\)$/, "\\1")
+    html = make_request "http://www.linkedin.com/countserv/count/share", url: url, callback: "IN.Tags.Share.handleCount"
     return JSON.parse(html)['count']
   end
 
   def self.googleplus url
-    begin
-      url = "https://plusone.google.com/_/+1/fastbutton?url=" + URI::encode(url)
-      html = RestClient.get url
-      return Nokogiri::HTML.parse(html).xpath('//*[@id="aggregateCount"]').text.to_i
-    rescue Exception => e
-      puts e
-    end
+    html = make_request "https://plusone.google.com/_/+1/fastbutton", url: url
+    return Nokogiri::HTML.parse(html).xpath('//*[@id="aggregateCount"]').text.to_i
   end
 
-  # def self.all url
-  #   supported_networks.inject({}) { |r, c| r[c.to_sym] = ShareCounts.send(c, url); r }
-  # end
+  # helpers - get all or selected networks
 
-  # def self.selected url, selections
-  #   selections.map{|name| name.downcase}.select{|name| supported_networks.include? name.to_s}.inject({}) {
-  #      |r, c| r[c.to_sym] = ShareCounts.send(c, url); r }
-  # end
+  def self.supported_networks
+    %w(reddit twitter facebook linkedin googleplus)
+  end
+
+  def self.all url
+    supported_networks.inject({}) { |r, c| r[c.to_sym] = ShareCounter.send(c, url); r }
+  end
+
+  def self.selected url, selections
+    selections.map{|name| name.downcase}.select{|name| supported_networks.include? name.to_s}.inject({}) {
+       |r, c| r[c.to_sym] = ShareCounter.send(c, url); r }
+  end
 
 end
 
 
+# tests, lol. they all pass.
+
 # url = "http://makeshift.io/"
-# puts ShareCounter.googleplus url
+# puts ShareCounter.all url
+# puts ShareCounter.selected url, [:linkedin, :facebook]
+
 # puts ShareCounter.linkedin url
+# puts ShareCounter.googleplus url
 # puts ShareCounter.twitter url
 # puts ShareCounter.facebook url
-
-
+# puts ShareCounter.reddit url
