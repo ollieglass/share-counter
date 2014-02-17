6381d1056c01fc068fefac6ca29a3ff44025d0f7
Ollie Glass <ollieglaskovik@gmail.com>
2014-02-17 10:30:53 +0000

    delicious, stumbleupon and pinterest lookups

--- lib/share-counter.rb
+++ lib/share-counter.rb
@@ -37,6 +37,25 @@ class ShareCounter
     return Nokogiri::HTML.parse(html).xpath('//*[@id="aggregateCount"]').text.to_i
   end
 
+  def self.delicious url
+    html = make_request "http://feeds.delicious.com/v2/json/urlinfo/data", url: url
+    puts html
+    # return Nokogiri::HTML.parse(html).xpath('//*[@id="aggregateCount"]').text.to_i
+  end
+
+  def self.stumbleupon url
+    html = make_request "http://www.stumbleupon.com/services/1.01/badge.getinfo", url: url
+    puts html
+    # return Nokogiri::HTML.parse(html).xpath('//*[@id="aggregateCount"]').text.to_i
+  end
+
+  def self.pinterest url
+    html = make_request "http://widgets.pinterest.com/v1/urls/count.json", url: url, source: 6
+    puts html
+    # return Nokogiri::HTML.parse(html).xpath('//*[@id="aggregateCount"]').text.to_i
+  end
+
+
   # helpers - get all or selected networks
 
   def self.supported_networks
@@ -66,3 +85,6 @@ end
 # puts ShareCounter.twitter url
 # puts ShareCounter.facebook url
 # puts ShareCounter.reddit url
+# puts ShareCounter.delicious url
+# puts ShareCounter.stumbleupon url
+# puts ShareCounter.pinterest url
