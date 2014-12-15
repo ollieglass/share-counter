dcd6d761f8dd2ca61f4bdd3b3c59d71f6f685a23
Ollie Glass <ollieglaskovik@gmail.com>
2014-12-15 15:45:23 +0000

    actual tests

--- spec/share-counter_spec.rb
+++ spec/share-counter_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
 
 describe ShareCounter do
-  it 'does stuff' do
-    pending # no code yet
+  let(:url) { "http://rinse.fm/"}
+
+  it 'looks up shares on each network' do
+    ShareCounter.supported_networks.each do |network|
+      puts "Looking up network: #{network}"
+      puts "Shares: #{ShareCounter.send(network, url)}"
+    end
   end
 end
